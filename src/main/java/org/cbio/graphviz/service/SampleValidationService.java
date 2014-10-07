498f5a4a95230d8526e2569b109d893fb027b702
Onur Sumer <s.onur.sumer@gmail.com>
2014-10-07 14:20:43 -0400

    sample input validator is now functional

--- src/main/java/org/cbio/graphviz/service/SampleValidationService.java
+++ src/main/java/org/cbio/graphviz/service/SampleValidationService.java
@@ -3,10 +3,14 @@ package org.cbio.graphviz.service;
 import flexjson.JSONSerializer;
 import org.springframework.core.io.Resource;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 /**
- * Service to retrieve custom study data as a graph.
+ * Service to validate input sample set.
  *
  * @author Selcuk Onur Sumer
  */
@@ -26,41 +30,45 @@ public class SampleValidationService
 	protected Set<String> pancanSamples = null;
 
 
-	protected List<String> getValidSamples(String[] samples)
+	protected List<String> getValidSamples(String[] samples) throws IOException
 	{
 		List<String> list = new ArrayList<String>();
 
+		Set<String> validSamples = new HashSet<String>();
 		Set<String> pancanSamples = this.getPancanSamples();
 
 		for (String sample: samples)
 		{
 			if (pancanSamples.contains(sample))
 			{
-				list.add(sample);
+				validSamples.add(sample);
 			}
 		}
 
+		list.addAll(validSamples);
 		return list;
 	}
 
-	protected List<String> getInvalidSamples(String[] samples)
+	protected List<String> getInvalidSamples(String[] samples) throws IOException
 	{
 		List<String> list = new ArrayList<String>();
 
+		Set<String> invalidSamples = new HashSet<String>();
 		Set<String> pancanSamples = this.getPancanSamples();
 
 		for (String sample: samples)
 		{
 			if (!pancanSamples.contains(sample))
 			{
-				list.add(sample);
+				invalidSamples.add(sample);
 			}
 		}
 
+		list.addAll(invalidSamples);
 		return list;
 	}
 
-	public String validateSamples(String samples)
+	public String validateSamples(String samples) throws IOException
 	{
 		JSONSerializer jsonSerializer = new JSONSerializer().exclude("*.class");
 
@@ -76,13 +84,30 @@ public class SampleValidationService
 		return jsonSerializer.deepSerialize(map);
 	}
 
-	protected  Set<String> getPancanSamples()
+	protected  Set<String> getPancanSamples() throws IOException
 	{
 		if (this.pancanSamples == null)
 		{
 			Set<String> samples = new HashSet<>();
 
-			// TODO read the pancan data resource and create a set of samples...
+			File input = this.getPancanDataResource().getFile();
+			BufferedReader reader = new BufferedReader(new FileReader(input));
+
+			String line;
+
+			// read the pancan data resource and create a set of samples
+			while ((line = reader.readLine()) != null)
+			{
+				// skip empty lines
+				if (line.length() == 0)
+				{
+					continue;
+				}
+
+				String parts[] = line.split("\t");
+				// assuming parts[0] is the case id...
+				samples.add(parts[0].trim());
+			}
 
 			this.pancanSamples = samples;
 		}
